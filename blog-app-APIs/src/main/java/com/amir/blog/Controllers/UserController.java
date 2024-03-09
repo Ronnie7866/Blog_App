8de24c8756266808c7970e327110d9491668e269
Mohammad Amir <mohdamir7505@gmail.com>
2024-03-09 00:40:26 -0800

    User APIs

--- blog-app-APIs/src/main/java/com/amir/blog/Controllers/UserController.java
+++ blog-app-APIs/src/main/java/com/amir/blog/Controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.amir.blog.Controllers;
 
+import com.amir.blog.Payloads.ApiResponse;
 import com.amir.blog.Payloads.UserDTO;
 import com.amir.blog.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,5 +22,29 @@ public class UserController {
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         UserDTO user = userService.createUser(userDTO);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable Integer userId) {
+        UserDTO updatedUser = userService.updateUser(userDTO, userId);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+        userService.deleteUser(userId);
+//        return new ResponseEntity<>(Map.of("message", "User Deleted Successfully"), HttpStatus.OK);
+        // if we dont want to use ApiResponse class we can use Map
+        return new ResponseEntity<>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<UserDTO> getSingleUser(@PathVariable Integer userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 }
